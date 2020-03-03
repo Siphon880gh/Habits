d4932599356d2ea20dc5e0b45b0fe641508cc9c9
Weng Fei Fung <weffung@ucdavis.edu>
2020-03-02 22:34:16 -0800

    Corrected colored chains: Now accounts for desired consecutive chains count

--- comps/log.js
+++ comps/log.js
@@ -166,23 +166,30 @@ function paintChains() {
     // Color chain icons that are consecutive
     var $queueChains = new Array();
     var $activeChains = $(".chain-icon.active").toArray();
+    var goalConsecutiveChains = $(".msgs-container-goal .mts-msg.active").text();
+    goalConsecutiveChains = parseInt(goalConsecutiveChains);
+    console.log("goalConsecutiveChains" + goalConsecutiveChains + "<< Expect 1-8");
+    if(isNaN(goalConsecutiveChains)) { 
+        console.error("Error goalConsecutiveChains is not an integer."); 
+        return false; 
+    };
     for(var i = $activeChains.length; i>=0; i--) { // Right to left
         var $chain = $( $activeChains[i] );
-        if($chain.data("visited")) continue;
+        if($chain.data("visited")) continue; // temp flag
 
         var $prevActiveChain = $chain.closest(".log").prev(".log").find(".chain-icon.active");
         var hasPrevActiveChain = $prevActiveChain.length;
         if(hasPrevActiveChain) {
             $queueChains.push($chain);
         } else {
-            if($queueChains.length>1) {
-                $queueChains.push($chain);
-                var $firstChained = $chain;
-                var $firstChainedLog = $firstChained.closest(".log");
-                var color = $firstChainedLog.css("border-top-color");
+            if($queueChains.length>=goalConsecutiveChains) {
+                $queueChains.push($chain); // on the left most active chain icon
+                var $leftMostChain = $chain;
+                var $leftMostChainLog = $leftMostChain.closest(".log");
+                var color = $leftMostChainLog.css("border-top-color"); // color of the consecutive chain icons will be in the color of the group of the left-most chain which started the consecutive chain
                 for(var j=0; j<$queueChains.length; j++) {
                     $queueChains[j].css("color", color);
-                    $queueChains[j].data("visited", 1);
+                    $queueChains[j].data("visited", 1); // temp flag
                     console.log("Changed chain icon color"); // To bring speed down from O(N^2).
                 } // for
             } // if there are consecutive chains and we are at the end of it, color the consecutive chain per the group color
