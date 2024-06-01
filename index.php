bcd55145039557c4f6b4b90efd69b0cf8aa307d6
Siphon880gh <weng.f.fung@gmail.com>
2024-05-31 20:57:19 -0700

    Adjusted cdns

--- index.php
+++ index.php
@@ -23,11 +23,11 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 
     <!-- jQuery and Bootstrap  -->
-    <link rel="stylesheet" href="../../offline/assets/bootstrap.min.css" data-old="../../offline/assets/bootstrap.min.css" data-old="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <script src="../../offline/assets/jquery-2.1.4.min.js" data-old="../../offline/assets/jquery-2.1.4.min.js" data-old="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-    <script src="../../offline/assets/handlebars.js" data-old="../../offline/assets/handlebars.js" data-old="https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.js"></script>
-    <script src="../../offline/assets/jquery.livequery.js" data-old="../../offline/assets/jquery.livequery.js" data-old="https://raw.githack.com/hazzik/livequery/master/src/jquery.livequery.js"></script>
-    <script src="../../offline/assets/moment.min.js" data-old="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.js"></script>
+    <script src="//raw.githack.com/hazzik/livequery/master/src/jquery.livequery.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
 
     <script src="vendors/multi-text-states.js"></script>
     <link href="vendors/multi-text-states.css" rel="stylesheet">
@@ -118,30 +118,30 @@ session_start();
         <!-- Designer: Open Sans, Lato, FontAwesome, Waypoints, Skrollr, Pixel-Em-Converter -->
         <!-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300|Open+Sans+Condensed:300" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"> -->
-        <link rel="stylesheet" href="../../offline/assets/font-awesome.css" data-old="../../offline/assets/font-awesome.css" data-old="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.css">
-        <script src="../../offline/assets/jquery.waypoints.min.js" data-old="../../offline/assets/jquery.waypoints.min.js" data-old="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.0/jquery.waypoints.min.js"></script>
-        <script src="../../offline/assets/skrollr.min.js" data-old="../../offline/assets/skrollr.min.js" data-old="https://cdnjs.cloudflare.com/ajax/libs/skrollr/0.6.30/skrollr.min.js"></script>
-        <script src="../../offline/assets/pxem.jQuery.js" data-old="../../offline/assets/pxem.jQuery.js" data-old="https://raw.githack.com/filamentgroup/jQuery-Pixel-Em-Converter/master/pxem.jQuery.js"></script>
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.css">
+        <script src="//cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.0/jquery.waypoints.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/skrollr/0.6.30/skrollr.min.js"></script>
+        <script src="//raw.githack.com/filamentgroup/jQuery-Pixel-Em-Converter/master/pxem.jQuery.js"></script>
         
         <!-- Rendering: Handlebars JS, Sprintf JS -->
-        <script src="../../offline/assets/handlebars.js" data-old="../../offline/assets/handlebars.js" data-old="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.5/handlebars.js"></script>
-        <script src="../../offline/assets/jquery.sprintf.js" data-old="../../offline/assets/jquery.sprintf.js" data-old="https://raw.githack.com/azatoth/jquery-sprintf/master/jquery.sprintf.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.5/handlebars.js"></script>
+        <script src="//raw.githack.com/azatoth/jquery-sprintf/master/jquery.sprintf.js"></script>
         
         <!-- Compatibility: Modernizr, jQuery Migrate (check browser) -->
-        <script src="../../offline/assets/modernizr.min.js" data-old="../../offline/assets/modernizr.min.js" data-old="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
-        <script src="../../offline/assets/jquery-migrate-1.2.1.min.js" data-old="../../offline/assets/jquery-migrate-1.2.1.min.js" data-old="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+        <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
         
         <!-- Mobile: jQuery UI, jQuery UI Touch Punch -->
-        <link href="../../offline/assets/jquery-ui.css" data-old="../../offline/assets/jquery-ui.css" data-old="https://code.jquery.com/ui/1.11.3/themes/ui-lightness/jquery-ui.css" rel="stylesheet"/>
-        <script src="../../offline/assets/jquery-ui.min.js" data-old="../../offline/assets/jquery-ui.min.js" data-old="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-        <script src="../../offline/assets/jquery.ui.touch-punch.min.js" data-old="../../offline/assets/jquery.ui.touch-punch.min.js" data-old="https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js"></script>
+        <link href="//code.jquery.com/ui/1.11.3/themes/ui-lightness/jquery-ui.css" rel="stylesheet"/>
+        <script src="//code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js"></script>
        
         <!-- Bootstrap JS -->
-        <script src="../../offline/assets/bootstrap.min.js" data-old="../../offline/assets/bootstrap.min.js" data-old="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         
         <!-- Friendlier API: ListHandlers, Timeout -->
-        <script src="../../offline/assets/jquery-handler-toolkit.js" data-old="../../offline/assets/jquery-handler-toolkit.js" data-old="https://raw.githack.com/Inducido/jquery-handler-toolkit.js/master/jquery-handler-toolkit.js"></script>
-        <script src="../../offline/assets/jquery.timeout.js" data-old="../../offline/assets/jquery.timeout.js" data-old="https://raw.githack.com/tkem/jquery-timeout/master/src/jquery.timeout.js"></script>
+        <script src="//raw.githack.com/Inducido/jquery-handler-toolkit.js/master/jquery-handler-toolkit.js"></script>
+        <script src="//raw.githack.com/tkem/jquery-timeout/master/src/jquery.timeout.js"></script>
 
     </body>
 </html>
